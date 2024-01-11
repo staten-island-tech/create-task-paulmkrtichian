199eb9698d3d834e7182ed038c515c7c25b49bff
paulmkrtichian <paulmkr22@gmail.com>
2024-01-11 09:06:10 -0500

    1/11

--- index.js
+++ index.js
@@ -15,16 +15,14 @@ async function getData(URL) {
   }
 } */
 
-const URL ="https://official-joke-api.appspot.com/jokes/random";
+const URL ="https://dog.ceo/api/breeds/image/random";
     async function getData(URL){
         try{
          const response = await fetch(URL);
-        if(response.status != 200){
-            throw new Error(response.statusText)
-        }
         console.log(response);
          const data = await response.json();
          console.log(data);
          } catch (error) {
+          console.log(error)
          }
     }
